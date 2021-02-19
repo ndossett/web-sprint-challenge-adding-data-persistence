44eb83d28fca2614078b320d8f2e920e43a66a09
ndossett <niki.m.dossett@gmail.com>
2021-02-19 11:10:03 -0500

    set up index, server, routers and models

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+module.exports = {
+    get,
+    create
+}
+
+function get(){
+    return db("resources")
+}
+function create(resource){
+    return db("resources").insert(resource)
+        .then(([id]) => {
+            return db("resources").where("id", id).first();
+        });
+}
